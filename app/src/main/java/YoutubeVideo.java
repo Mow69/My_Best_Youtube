f3082fa4f612b2cf481ed0785c987213df276948
Mow <mowster69@outlook.com>
2022-04-01 15:38:28 +0200

    feat/add-video : add POJO

---
+++ app/src/main/java/YoutubeVideo.java
@@ -0,0 +1,71 @@
+import java.io.Serializable;
+
+public class YoutubeVideo implements Serializable {
+    private Long id;
+    private String titre;
+    private String description;
+    private String url;
+    private String categorie;
+
+    public YoutubeVideo() {
+    }
+
+    public YoutubeVideo(Long id, String titre, String description, String url, String categorie) {
+        this.id = id;
+        this.titre = titre;
+        this.description = description;
+        this.url = url;
+        this.categorie = categorie;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    @Override
+    public String toString() {
+        return "YoutubeVideo{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", description='" + description + '\'' +
+                ", url='" + url + '\'' +
+                ", categorie='" + categorie + '\'' +
+                '}';
+    }
+}
