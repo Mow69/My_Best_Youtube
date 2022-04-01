994e91c9f63a36e69e109cedca55369321f32714
Mow <mowster69@outlook.com>
2022-04-01 17:46:15 +0200

    feat/list-videos : update onCreate

--- app/src/main/java/com/ita/mybestyoutube/MainActivity.java
+++ app/src/main/java/com/ita/mybestyoutube/MainActivity.java
@@ -2,23 +2,44 @@ package com.ita.mybestyoutube;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Context context;
+    private TextView tvYoutubeVideos;
+    private Context context;
+    private RecyclerView rvYoutubeVideos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // ancienne vue pour la main
         setContentView(R.layout.activity_main);
 
-        // recupère le contexte
+        // nouvelle vue avec le recyclerview
+        setContentView(R.layout.recyclerview_main);
+
+        // ancien avec Textview
+        //tvYoutubeVideos = findViewById(R.id.tvYoutubeVideos);
+
+        // récupère le RecyclerView
+        rvYoutubeVideos = findViewById(R.id.rvYoutubeVideos);
+
+        // récupère le context
         context = getApplicationContext();
+
+        // créé le LayoutManager pour manager le RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        rvYoutubeVideos.setHasFixedSize(true);
+        // lie le LayoutManager avec le RecyclerView
+        rvYoutubeVideos.setLayoutManager(layoutManager);
     }
 
     // recupère le menu
